f7ee5edc8fae70f311f995aee3653dbda2af9bf8
Allie Jones <allie@alliejon.es>
2021-12-04 20:32:26 -0500

    Add deps files

--- util/read-input.ts
+++ util/read-input.ts
@@ -1,10 +1,12 @@
-import { join } from "https://deno.land/std/path/mod.ts";
+import { path } from "../deps.ts";
 
 export default async function readInput<T>(
   lineParser: (line: string) => T,
   filename: string,
 ): Promise<T[]> {
-  const file = await Deno.readTextFile(join(Deno.cwd(), `${filename}.txt`));
+  const file = await Deno.readTextFile(
+    path.join(Deno.cwd(), `${filename}.txt`),
+  );
   return file.toString().split("\n").filter((line) => line !== "").map((item) =>
     lineParser(item)
   );
